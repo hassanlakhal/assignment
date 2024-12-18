396fdcaeb9b82e28e30438b65b76f9440a9f9c3d
Hassan Lakhal <hlakhal-@c3r5p7.1337.ma>
2024-12-18 15:15:45 +0100

    create class DeribitClient and lib  build-essential for request API and fetching of data

--- src/DeribitClient.cpp
+++ src/DeribitClient.cpp
@@ -1,3 +1,9 @@
-// int main(){
+#include "DeribitClient.h"
 
-// }
+DeribitClient::DeribitClient(std::string client_id, std::string url, std::string client_secret){
+
+}
+
+std::string DeribitClient::sendRequest(std::string url, std::string methods, std::string paylod){
+
+}
--- src/DeribitClient.h
+++ src/DeribitClient.h
@@ -1,4 +1,15 @@
+#include<string>
+#include <curl/curl.h>
+
+
+
 class DeribitClient{
-  
-    
+    private:
+        std::string client_id;
+        std::string url;
+        std::string client_secret; 
+        std::string sendRequest(std::string url, std::string methods, std::string paylod);
+    public:
+        DeribitClient(std::string client_id, std::string url, std::string client_secret);
+
 };
