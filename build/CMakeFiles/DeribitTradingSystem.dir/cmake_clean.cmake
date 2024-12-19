d517c01962522b9b5f3e403fdb23415017cd7aa2
lakhalhassan <lakehalhassan2120@gmail.com>
2024-12-19 11:38:14 +0100

    fixed error in cmake

---
+++ build/CMakeFiles/DeribitTradingSystem.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DeribitTradingSystem.dir/src/DeribitClient.cpp.o"
+  "CMakeFiles/DeribitTradingSystem.dir/src/DeribitClient.cpp.o.d"
+  "CMakeFiles/DeribitTradingSystem.dir/src/main.cpp.o"
+  "CMakeFiles/DeribitTradingSystem.dir/src/main.cpp.o.d"
+  "DeribitTradingSystem"
+  "DeribitTradingSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DeribitTradingSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
